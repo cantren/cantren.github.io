6d779e889d97e5ffe16e33065fb237f2fb60da93
cantren <nickcantrell@gmail.com>
2015-03-14 10:58:28 -0700

    Update FirstPersonControls.js

--- js/FirstPersonControls.js
+++ js/FirstPersonControls.js
@@ -7,7 +7,7 @@
 THREE.FirstPersonControls = function ( object, domElement ) {
 
 	this.object = object;
-	this.target = new THREE.Vector3( 0, 0, 0 );
+	this.target = new THREE.Vector3( 0, 90, 0 );
 
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
 
