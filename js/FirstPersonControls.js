7d349d047341b1c68719e224f46b2f69b3123fc7
cantren <nickcantrell@gmail.com>
2016-09-08 13:11:06 -0700

    Update FirstPersonControls.js

--- js/FirstPersonControls.js
+++ js/FirstPersonControls.js
@@ -204,9 +204,9 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		}
 
-		var actualMoveSpeed = velocity;
+		var actualMoveSpeed = this.velocity;
 
-		if ( this.moveForward || ( this.autoForward && !this.moveBackward ) ){ var velocity += delta * this.movementSpeed; this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) )};
+		if ( this.moveForward || ( this.autoForward && !this.moveBackward ) ){ this.velocity += (delta * this.movementSpeed); this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) )};
 		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
 
 		if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
